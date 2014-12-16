1cbfb7f89d936bfd93451d843bd0f32d44e9cf9f
Billy.Zheng <zw963@163.com>
2014-12-16 19:57:48 +0800

    change version.rb to conform to ritual

--- lib/erb_to_slim/version.rb
+++ lib/erb_to_slim/version.rb
@@ -1,5 +1,12 @@
- # -*- coding: utf-8 -*-
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 
- module ErbToSlim
-   VERSION = '0.0.7'
- end
+module ErbToSlim
+  VERSION = [0, 0, 7]
+
+  class << VERSION
+    def to_s
+      join(?.)
+    end
+  end
+end
